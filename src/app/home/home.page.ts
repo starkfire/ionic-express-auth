c5a806a9015e4a885644132057235d7ca2bea1f2
Pocholo <plpantoja49@gmail.com>
2021-01-26 05:44:12 +0800

    display log entries (the simplest way)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LogsService } from '../logs/logs.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  public results: Array<object> = []
 
   constructor(
-    private router: Router
+    private router: Router,
+    private logs: LogsService
   ) {}
+
+  ngOnInit() {
+    this.logs.getLogs()
+      .subscribe(resp => {
+        this.results = resp.body.logs
+      }, err => {
+        console.log(err)
+      })
+  }
 
   logout(): void {
     localStorage.removeItem('token')
